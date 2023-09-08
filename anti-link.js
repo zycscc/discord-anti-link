55deb224deae1ec2c14736232a910979736cecdf
Zycs <104656817+zycscc@users.noreply.github.com>
2023-09-08 23:52:00 +0300

    Update anti-link.js

--- anti-link.js
+++ anti-link.js
@@ -4,7 +4,7 @@ client.on('message', (message) => {
 if (message.content.includes('discord.gg/'||'discordapp.com/invite/'||'https://')) {
     if(!message.member.permissions.has('ADMINISTRATOR')) { 
     message.delete()
-      .then(message.reply("Link Deleted: **Links are forbidded!**"))
+      .then(message.reply("Link Deleted: **Links are forbidden!**"))
    }
 }
 })
