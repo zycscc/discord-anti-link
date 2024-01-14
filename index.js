ef22e574df4aeab97892ffd928915834b6e907ea
Zycs <104656817+zycscc@users.noreply.github.com>
2024-01-14 17:14:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
-const antilink = require("./anti-link");
-antilink(client);
-
+client.on('message', (message) => {
+if (message.content.includes('discord.gg/'||'discordapp.com/invite/'||'https://')) {
+    if(!message.member.permissions.has('ADMINISTRATOR')) { 
+    message.delete()
+      .then(message.reply("Link Deleted: **Links are forbidden!**"))
+   }
+}
+})
 
 client.login(config.token);
